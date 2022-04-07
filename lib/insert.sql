ee77bf0737fa7fc3bd76600427a64d1061dddf93
wesley <wesbourkeavl@gmail.com>
2022-04-07 14:27:30 -0400

    commit tree 992ca94417858748738fda9814ae4037e68199b7

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) Values ("Mr. Chocolate", 32, "M", "Brown", "He does well", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) Values ("Rowdy", 26, "F", "Black", "He does well", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) Values ("Tabitha", 30, "F", "Yellow", "He does well", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) Values ("Sergeant Brown", 27, "M", "Brown", "He does well", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) Values ("Melissa", 27, "M", "Brown", "He does well", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) Values ("Grinch", 27, "f", "Black", "He does well", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) Values ("Wendy", 27, "M", "Brown", "He does well", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) Values (NULL, 27, "f", "Yellow", "He does well", true);
